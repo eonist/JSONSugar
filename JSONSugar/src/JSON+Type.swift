d3b039bf8caced847f995896a696c23da71bc448
andre.jorgensen <andre.jorgensen@tidal.com>
2019-09-04 11:40:08 +0200

    Add one-liner examples to readme

--- JSONSugar/src/JSON+Type.swift
+++ JSONSugar/src/JSON+Type.swift
@@ -6,26 +6,20 @@ public enum JSONType {
     case arr
     case int
     case dict
-    case dictArr/*An array of dictionaries*/
+    case dictArr /* An array of dictionaries */
     case str
 }
 extension JSONType {
     public static func type(_ json: Any?) -> JSONType {
         switch true {
-        case JSONAsserter.isStr(json):
-            return .str
-        case JSONAsserter.isInt(json):
-            return .int
-        case JSONAsserter.isDict(json):
-            return .dict
-        case JSONAsserter.isDictArr(json):
-            return .dictArr
-        case JSONAsserter.isArr(json):
-            return .arr
+        case JSONAsserter.isStr(json): return .str
+        case JSONAsserter.isInt(json): return .int
+        case JSONAsserter.isDict(json): return .dict
+        case JSONAsserter.isDictArr(json): return .dictArr
+        case JSONAsserter.isArr(json): return .arr
         default:
             print("Type not supported: \(Swift.type(of: json))")
             fatalError("Type not supported: \(Swift.type(of: json))")
-//            break
         }
     }
 }
--- JSONSugar/src/JSONUtils.swift
+++ JSONSugar/src/JSONUtils.swift
@@ -7,15 +7,15 @@ public class JSONUtils {
     public static func describe(_ json: Any?) {
         if let str = JSONParser.str(json) {
             Swift.print("Str: \(str)")
-        }else if let int = JSONParser.int(json) {
+        } else if let int = JSONParser.int(json) {
             Swift.print("Int: \(int)")
-        }else if let dict = JSONParser.dict(json) {
+        } else if let dict = JSONParser.dict(json) {
             Swift.print("Dict.count: " + "\(dict.count)")
             dict.forEach { key, value in
                 Swift.print("key: \(key)")
                 describe(value)
             }
-        }else if let arr = JSONParser.arr(json) {
+        } else if let arr = JSONParser.arr(json) {
             Swift.print("Arr.count: " + "\(arr.count)")
             arr.forEach { child in
                 describe(child)
