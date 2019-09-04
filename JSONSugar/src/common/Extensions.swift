1f21b37fc4675b0ad832f61c47892b79dc54792c
andre.jorgensen <andre.jorgensen@tidal.com>
2019-09-04 10:14:40 +0200

    Add convenient decode method

---
+++ JSONSugar/src/common/Extensions.swift
@@ -0,0 +1,23 @@
+import Foundation
+/**
+ * Data
+ */
+extension Data {
+   /**
+    * let strings: [String]? = try ? "["a","b","c"]".data(using: .utf8)?.decode()
+    */
+   public func decode<T: Decodable>() throws -> T {
+      let decoder = JSONDecoder()
+      return try decoder.decode(T.self, from: self)
+   }
+}
+extension String {
+   /**
+    * let strings: [String]? = try ? "["a","b","c"]".decode()
+    */
+   public func decode<T: Decodable>(encoding: String.Encoding = .utf8) throws -> T {
+      let decoder = JSONDecoder()
+      guard let data: Data = self.data(using: encoding) else { throw NSError.init(domain: "unable to convert string to data", code: 0) }
+      return try decoder.decode(T.self, from: data)
+   }
+}
