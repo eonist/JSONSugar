9f18dea63de307eb44c99c3b8442996b1e81b157
André J <30n1st@gmail.com>
2021-10-03 05:00:30 +0200

    Create Codable+Ext.swift

---
+++ Sources/JSONSugar/common/Codable+Ext.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+extension Encodable {
+   /**
+    * Struct -> Dictionary
+    * ## Examples:
+    * struct Test { let a: String, b }
+    * Test(a: "1", b: "2").dict // ["a": "1", "b": "2"]
+    */
+   public var dict: [String: Any]? {
+      guard let data = try? JSONEncoder().encode(self) else { return nil }
+      return (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)).flatMap { $0 as? [String: Any] }
+   }
+}
+extension Decodable {
+   /**
+    * Dictonary -> Struct
+    * ## Examples:
+    * let test = Test(dict: ["a": "1", "b": "2"])
+    */
+   public init(dict: [String: Any]) throws {
+      self = try JSONDecoder().decode(Self.self, from: JSONSerialization.data(withJSONObject: dict))
+   }
+}
